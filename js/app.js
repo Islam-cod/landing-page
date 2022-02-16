f29898850edcb32eee8a43351939b2dd23dd8695
Islam-cod <74316365+Islam-cod@users.noreply.github.com>
2022-02-16 17:42:36 +0100

    Udacity landing page project

--- js/app.js
+++ js/app.js
@@ -1,91 +1,74 @@
-//Build the NavBar dynamically
-let navLinks = document.getElementById("unordered-list");
-let sectionsCount = document.getElementsByClassName("landing__container").length;
 
-for (i = 1; i < sectionsCount + 1; i++) {
-  let item = "#section" + i + "-title";
-  let itemValue = document.querySelector(item);
-  let itemText = itemValue.textContent;
-  let newLine = document.createElement("li");
-  let lineText = document.createTextNode(itemText);
-  let listItem = "sample-nav-" + i;
-  newLine.setAttribute("id", listItem);
-  newLine.setAttribute("class", "nav-list-item");
-  newLine.appendChild(lineText);
-  navLinks.appendChild(newLine);
-//Create a button for every section
-  let itemTarget = document.getElementById("section" + i);
-  let listTarget = document.getElementById(listItem);
-  let buttonName = "section" + i + "-button";
-  let buttonToAdd = document.getElementById(buttonName);
+//Build NavBar dynamically
+const NavBar = document.getElementById("unordered-list")
+let sectionList = document.querySelectorAll("section");
 
-  //Adds button when scrolls into view
-
-  listTarget.addEventListener("click", function() {
-    itemTarget.scrollIntoView ()
-    buttonToAdd.innerHTML =
-      "<button class='section-button' onclick='goToTop()'>Return to Top</button>";
-  });
-}
-// A helper function
-const scrollToTop = () => {
-  const scrolling = document.documentElement.scrollTop || document.body.scrollTop;
-  if (scrolling > 0) {
-    window.requestAnimationFrame(scrollToTop);
-    window.scrollTo(0, scrolling - scrolling / 50);
-  }
-};
+//Build a "scroll to top" button
+const body = document.querySelector("body")
+const topButton= document.createElement("button");
+topButton.innerHTML= `<button onclick="topFunction()" id="topBtn" title="Go to top">Return to Top</button>`
+body.appendChild(topButton);
 
 
-// On button click, goes to top of page 
-function goToTop() {
-scrollToTop();
-  for (i = 1; i < sectionsCount + 1; i++) {
-    let buttonToDelete = document.getElementById("section" + i + "-button");
-    buttonToDelete.innerHTML = "";
-  }
+function getSectionId(section){
+  return section.id
 }
 
-// Add active-class with styling changes
-function checkIfSectionInView() {
-  let isInViewport = function(elem) {
-    let bounding = elem.getBoundingClientRect();
+function getSectionTitle(section){
+    return section.querySelector(".title").innerText
+  }
+
+function CreateNavLink(sectionId, sectionTitle){
+    const liElement = document.createElement("li")
+    liElement.innerHTML = `<a href ="#${sectionId}" class="nav-link"> ${sectionTitle} </a>`
+    liElement.classList.add("nav-item")
+    NavBar.appendChild(liElement)
+  }
+
+// When the user scrolls down, show the button
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0;
+  }
+
+sectionList.forEach(section => {
+    const sectionId = getSectionId(section)
+    const sectionTitle = getSectionTitle(section)
+    CreateNavLink(sectionId, sectionTitle)
+    })
+
+
+// Add class 'active' to section when near top of viewport
+
+let isInViewport = function(elem) {
+    let distance = elem.getBoundingClientRect();
     return (
-      bounding.top <= 50 &&
-      bounding.bottom <=
-        (window.innerHeight || document.documentElement.clientHeight) &&
-      bounding.right <=
-        (window.innerWidth || document.documentElement.clientWidth)
+      distance.top >= 0 &&
+      distance.left >= 0 &&
+      distance.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      distance.right <= (window.innerWidth || document.documentElement.clientWidth)
     );
   };
+  
+  let findMe = document.querySelectorAll("section");
+  window.addEventListener('scroll', function(event) {
+  findMe.forEach(element => {
+      if (isInViewport(element)) {
+        element.classList.add("your-active-class");
+      } else {
+        element.classList.remove("your-active-class"); 
+      }
+  });
+  }, false);
 
-  for (i = 1; i < sectionsCount + 1; i++) {
-    let sectionInFullView = document.getElementById("section" + i);
-
-    window.addEventListener(
-      "scroll",
-      function(event) {
-        if (isInViewport(sectionInFullView)) {
-          sectionInFullView.classList.add("your-active-class");
-        } else {
-          sectionInFullView.classList.remove("your-active-class");
-        }
-      },
-      false
-    );
-  }
-}
-
-// Make the NavBar sticky
-window.onscroll = function() {staySticky()};
-let navbar = document.getElementById("header");
-let sticky = header.offsetTop;
-
-function staySticky() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
-checkIfSectionInView();
+mybutton = document.getElementById("topBtn");
+window.onscroll = function() {scrollFunction()};
